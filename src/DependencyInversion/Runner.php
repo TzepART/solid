b3f0f8154924774d76874d562eca5758fbafd81e
Artem Tsepkov <tzepart@gmail.com>
2022-04-01 03:39:45 +0300

    Add README.md

---
+++ src/DependencyInversion/Runner.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DependencyInversion;
+
+use App\RunnerInterface;
+
+class Runner implements RunnerInterface
+{
+    public function problemRun(): void
+    {
+        // TODO: Implement problemRun() method.
+    }
+
+    public function solutionRun(): void
+    {
+        // TODO: Implement solutionRun() method.
+    }
+}
